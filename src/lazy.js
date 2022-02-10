1fefcad4649861d71f47f2b031691e6c06adfdb8
Jose Guadalupe Cortes Cruz <joseegccr@gmail.com>
2022-02-09 22:55:21 -0600

    Aplicando Lazy Loading

--- src/lazy.js
+++ src/lazy.js
@@ -3,17 +3,21 @@ const isIntersecting = (entry) => {
     return entry.isIntersecting;
 };
 
-const accion = (entry) =>{
-    const nodo = entry.target
-    console.log("hola");
+const loadImage = (entry) =>{
+    const container = entry.target;
+    const imagen = container.firstChild;
+    const url = imagen.dataset.src;
+    imagen.src = url;
 
-    observer.unobserve(nodo)
+    //console.log("hola");
+
+    observer.unobserve(container)
 };
 
 const observer = new IntersectionObserver((entries)=>{
     entries
         .filter(isIntersecting)
-        .forEach(accion);
+        .forEach(loadImage);
 
 });
 
